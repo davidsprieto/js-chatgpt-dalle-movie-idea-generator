55e581a84296a440c334f070379f3d017e427a03
David Prieto <david.s.prieto1@gmail.com>
2023-08-11 14:27:07 -0500

    refactored fetch request to use openai's dependency

---
+++ env.js
@@ -0,0 +1,5 @@
+export const process = {
+    env: {
+        OPENAI_API_KEY: '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'
+    }
+}
--- index.js
+++ index.js
@@ -1,30 +1,29 @@
+import { process } from '/env';
+import { Configuration, OpenAIApi } from 'openai';
+
 const setupTextarea = document.getElementById('setup-textarea');
 const setupInputContainer = document.getElementById('setup-input-container');
 const movieBossText = document.getElementById('movie-boss-text');
 
-const apiKey = "";
-const url = "https://api.openai.com/v1/completions";
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
 
 document.getElementById("send-btn").addEventListener("click", () => {
     // if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading" alt="loading">`
+    setupInputContainer.innerHTML = `<img src="./images/loading.svg" class="loading" id="loading" alt="loading">`
     movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...`
     // }
     fetchBotReply();
 });
 
-function fetchBotReply() {
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify({
-            'model': 'text-davinci-003',
-            'prompt': 'Sound enthusiastic in 5 words or less.'
-        })
-    }).then(response => response.json()).then(data =>
-        movieBossText.innerText = data.choices[0].text
-    );
+async function fetchBotReply() {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: 'Sound enthusiastic in 5 words or less.'
+    });
+    movieBossText.innerText = response.data.choices[0].text.trim();
 }
+
